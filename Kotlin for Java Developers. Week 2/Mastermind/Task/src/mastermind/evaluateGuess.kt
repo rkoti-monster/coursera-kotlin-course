fd8af06e008e100344eac21dc707e668778c7d0f
Raghavendra Koti <raghavendra.koti@monster.com>
2020-09-11 11:45:21 +1000

    Use sequenceOf()

--- Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -6,11 +6,11 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     return Evaluation(
-            guess.zip(secret).count { it.first == it.second },
+            guess.asSequence().zip(secret.asSequence()).count { it.first == it.second },
             getCharFrequency(guess, secret).map { it.key to min(it.value, getCharFrequency(secret, guess).getOrDefault(it.key, 0)) }.toMap().values.sum())
 }
 
 fun getCharFrequency(first: String, second: String): Map<Char, Int> {
-    val diffs = first.withIndex().filter { x -> second.withIndex().none { it == x } }
-    return diffs.groupBy { it.value }.mapValues { it.value.size }
+    val diffs = first.asSequence().withIndex().filter { x -> second.withIndex().none { it == x } }
+    return diffs.asSequence().groupBy { it.value }.mapValues { it.value.size }
 }
